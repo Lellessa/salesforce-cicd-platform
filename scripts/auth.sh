5e6773a6b329b2fb1fe6b70abc529230174ac68e
Leonardo Lessa <lessa.leo20@gmail.com>
2026-06-15 13:38:15 -0300

    test: Actions

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -7,11 +7,9 @@ ENVIROMENT=${1:?Usage: auth.sh <dev|it|qa>}
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../config/enviroments/${ENVIROMENT}.env"
 
-echo $SECRET
-
 sf org login jwt \
   --client-id $CONSUMER_KEY \
-  --jwt-key-file $SECRET \
+  --jwt-key-file server.key \
   --username $USERNAME \
   --alias $ORG_ALIAS \
   --instance-url $URL
