767bc528229b2168d11a4fd3ec8f61f7f77a87d3
Leonardo Lessa <lessa.leo20@gmail.com>
2026-06-15 13:45:09 -0300

    test: Actions

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -7,7 +7,7 @@ ENVIROMENT=${1:?Usage: auth.sh <dev|it|qa>}
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../config/enviroments/${ENVIROMENT}.env"
 
-echo "${{ secrets.DEV }}" > server.key
+echo "$JWT_KEY" > server.key
 chmod 600 server.key
 
 sf org login jwt \
