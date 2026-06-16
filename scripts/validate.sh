ab95068208b9e094b05c2a9226d40d69fbdfacac
Leonardo Lessa <lessa.leo20@gmail.com>
2026-06-15 23:02:58 -0300

    test: Actions

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 set -euo pipefail
+source utils/common.sh
 
 goto_project
-
-source utils/common.sh
 
 sf project deploy validate --manifest ./manifest/package.xml --target-org $ORG_ALIAS
