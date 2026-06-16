c4ffcdde9b30b9dfc1708dc8c61c66de6183d344
Leonardo Lessa <lessa.leo20@gmail.com>
2026-06-16 14:13:42 -0300

    test: Actions

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -5,10 +5,6 @@ set -euo pipefail
 echo "$JWT_KEY" > server.key
 chmod 600 server.key
 
-echo "$ORG_ALIAS"
-echo "$USERNAME"
-echo "$CONSUMER_KEY"
-
 sf org login jwt \
   --client-id $CONSUMER_KEY \
   --jwt-key-file server.key \
