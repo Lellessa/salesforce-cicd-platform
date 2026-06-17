abe945275df30bb09aeeec21c8c55e32e3859bb8
Leonardo Lessa <lessa.leo20@gmail.com>
2026-06-16 22:06:47 -0300

    test: Actions

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -6,4 +6,6 @@ source ./scripts/utils/common.sh
 goto_project
 
 sf project deploy validate --manifest ./manifest/package.xml --target-org $ORG_ALIAS --test-level $TEST_LEVEL \
-    > "$GITHUB_WORKSPACE/validation.txt"
+    > validation.txt
+
+sed -n '/Status:/,$p' validation.txt > "$GITHUB_WORKSPACE/summary.txt"
