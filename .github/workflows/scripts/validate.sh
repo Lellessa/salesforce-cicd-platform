b29a0f6dca79c8cc753eabf69cec48b9216eafaf
Leonardo Lessa <lessa.leo20@gmail.com>
2026-07-10 23:18:20 -0300

    ci(validate): improving validate report comment on pr

--- .github/workflows/scripts/validate.sh
+++ .github/workflows/scripts/validate.sh
@@ -2,8 +2,5 @@
 
 set -euo pipefail
 
-NO_COLOR=1 sf project deploy validate --manifest ./manifest/package.xml --target-org $ORG_ALIAS --test-level $TEST_LEVEL \
-    > validation.txt
-
-cat validation.txt
-sed -n '/Status:/,$p' validation.txt > "$GITHUB_WORKSPACE/summary.txt"
+NO_COLOR=1 sf project deploy validate --manifest ./manifest/package.xml --target-org $ORG_ALIAS --test-level $TEST_LEVEL --json \
+    > validate.json
